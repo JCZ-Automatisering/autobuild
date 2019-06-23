73e1b549e6d10ae85d97004b63f0c888e624a433
Jaap Crezee <jaap@jcz.nl>
2019-06-23 14:20:14 +0200

    process Jenkinsfile correctly, also "mount" HOME

--- autobuild.py
+++ autobuild.py
@@ -5,6 +5,7 @@ import sys
 import configparser
 
 CONFIG_FILE = "autobuild.ini"
+CONFIG_SECTION = "autobuild"
 
 
 def error(msg):
@@ -15,8 +16,13 @@ def error(msg):
 if not os.path.exists(CONFIG_FILE):
     error("config file %s does not exist" % CONFIG_FILE)
 
+cp = configparser.ConfigParser()
+cp.read(CONFIG_FILE)
+config = cp[CONFIG_SECTION]
 
-docker_name = "shared_c_build_local"
+docker_name = config['name']
+docker_file = config['dockerfile']
+docker_file_dir = os.path.dirname(docker_file)
 
 
 def execute(command):
@@ -38,14 +44,20 @@ def execute_in_docker(command):
     execute("cat %s | tail -n 1" % __tmp_name)
 
     command = "/bin/sh %s" % __tmp_name
-    docker_cmd = "docker run --rm -it --name %s -v $PWD:$PWD -w $PWD -u $(id -u) %s %s" % \
-                 (docker_name, docker_name, command)
+    HOME = os.getenv("HOME")
+    if not HOME:
+        error("HOME not set!")
+
+    HOME_VOL_AND_VAR = "-v %s:%s -e HOME=%s" % (HOME, HOME, HOME)
+    docker_base = "docker run --rm -it --name %s %s" % (docker_name, HOME_VOL_AND_VAR)
+    docker_cmd = "%s -v $PWD:$PWD -w $PWD -u $(id -u) %s %s" % \
+                 (docker_base, docker_name, command)
     execute(docker_cmd)
 
     os.unlink(__tmp_name)
 
 
-execute("cd docker && docker build -t %s ." % docker_name)
+execute("cd %s && docker build -t %s ." % (docker_file_dir, docker_name))
 execute("mkdir -p tmp")
 
 if len(sys.argv) > 1:
@@ -64,12 +76,17 @@ with open("Jenkinsfile", "r") as jf:
             split = line.split("'")
             stage = split[1]
 
-        if line.startswith("dockerImage.inside()"):
+        if line.startswith("dockerImage.inside("):
             line = jf.readline()    # get next line which contains shell command
             split = line.strip()[4:][:-2]
             steps.append({"name": stage, "command": split})
         line = jf.readline()
 
+
+if os.getenv("NO_BUILD"):
+    for item in steps:
+        print("Step: %s" % item['name'])
+    print("NO_BUILD set, stopping now")
 
 for item in steps:
     print("Step: %s" % item['name'])
